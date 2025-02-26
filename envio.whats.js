3b358c175e5b3330d4dcf861c2db7bde43c3ef7c
kaylaneuy <kaylane2anjos.designer@gmail.com>
2025-02-26 15:41:34 -0300

    chore: corrige link para redirecionamento de whatsapp np formulario

--- envio.whats.js
+++ envio.whats.js
@@ -3,8 +3,8 @@ function formulario() {
   const email = document.getElementById("formulario-email").value;
   const desc = document.getElementById("formulario-desc").value;
   console.log(nome, email, desc);
-  window.location.assign("dasjdkasdka");
+  window.location.assign(template(desc));
 }
 
 const template = (text) =>
-  `https://api.whatsapp.com/send?phone=5584998084621&te/xt=${text}}`;
+  `https://api.whatsapp.com/send?phone=5584998084621&te/xt=${text}`;
