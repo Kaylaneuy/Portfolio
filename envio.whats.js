920c3da1a0fb5a56cdc6546dcf308c7468dfcf29
kaylaneuy <kaylane2anjos.designer@gmail.com>
2025-02-26 15:36:41 -0300

    versao inicial

---
+++ envio.whats.js
@@ -0,0 +1,10 @@
+function formulario() {
+  const nome = document.getElementById("formulario-nome").value;
+  const email = document.getElementById("formulario-email").value;
+  const desc = document.getElementById("formulario-desc").value;
+  console.log(nome, email, desc);
+  window.location.assign("dasjdkasdka");
+}
+
+const template = (text) =>
+  `https://api.whatsapp.com/send?phone=5584998084621&te/xt=${text}}`;
